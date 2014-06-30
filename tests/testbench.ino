b01b1aab0924271c177b77b163cda95176c492e9
Joakim Børlum <joakim@roevhat.dk>
2014-06-30 20:36:21 +0200

    Tesbench example

--- tests/testbench.ino
+++ tests/testbench.ino
@@ -1,9 +1,28 @@
 #include <arduous.h>
 
+#define LED_PIN 13
+
+void task1(void) {
+    while (1) {
+        /* Gør noget her */
+        digitalWrite(LED_PIN, HIGH);
+    }
+}
+
+void task2(void) {
+    while (1) {
+        /* Gør noget her */
+        digitalWrite(LED_PIN, LOW);
+    }
+}
+
 void setup() {
     Serial.begin(9600);
+    pinMode(LED_PIN, OUTPUT);
+
+    ardk_create_thread(task1);
+    ardk_create_thread(task2);
     ardk_start();
 }
 
-void loop() {
-}
+void loop() {/* Move along */}
